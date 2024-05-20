6b82b0653d4a8cef7a12b5da8604991a630c08aa
jdascoli <joshuadascoli@gmail.com>
2024-05-20 03:05:53 -0500

    Working Chip Firing

--- FireChips.java
+++ FireChips.java
@@ -3,13 +3,16 @@
  * @author Josh Dascoli
  */
 public class FireChips {
+    public static final int ROWS = 69, COLS = 69;
 
     /**
      * Fire the Chips
      * @param args
      */
     public static void main(String args[]) {
-        Grid grid = new Grid(5, 5, 3, 2);
+        Grid grid = new Grid(ROWS, COLS, 0, 10000);
+        grid.printArray();
+        grid.ChipDFS(ROWS/2, COLS/2);
         grid.printArray();
     }
 }
--- Grid.java
+++ Grid.java
@@ -1,8 +1,9 @@
 
+
 public class Grid {
 
     private final int[][] grid;
-    private final int baseNum, centerNum;
+    private final int baseNum, centerNum, rows, columns;
 
     /**
      * @param rows rows of array
@@ -11,6 +12,8 @@ public class Grid {
      * @param centerNum center number
      */
     public Grid(int rows, int columns, int baseNum, int centerNum) {
+        this.rows = rows;
+        this.columns = columns;
         this.grid = new int[rows][columns];
         this.baseNum = baseNum;
         this.centerNum = centerNum;
@@ -21,14 +24,13 @@ public class Grid {
      * make a 2d array
      */
     private void makeGrid() {
-
         //make an array with a base amount of chips in each space;
         for (int[] grid1 : grid) {
-            for (int j = 0; j < grid1.length; j++) {
+            for (int j = 0; j < columns; j++) {
                 grid1[j] = baseNum;
             }
         }
-        grid[grid.length / 2][grid[0].length / 2] = centerNum;
+        grid[rows / 2][columns / 2] = centerNum;
     }
 
     /**
@@ -41,7 +43,9 @@ public class Grid {
             }
             System.out.println();
         }
+        System.out.println();
     }
+
     /**
      * @param number number
      * @return the number colored
@@ -49,11 +53,45 @@ public class Grid {
     private String colorNumber(int number) {
         String colorCode;
         colorCode = switch (number) {
-            case 1 -> "\u001B[36m";
-            case 2 -> "\u001B[33m";
-            case 3 -> "\u001B[31m";
-            default -> "\u001B[34m";
+            case 1 ->
+                "\u001B[36m";
+            case 2 ->
+                "\u001B[33m";
+            case 3 ->
+                "\u001B[31m";
+            default ->
+                "\u001B[34m";
         };
         return colorCode + number + "\u001B[0m";
+    }
+
+    /**
+     * DFS to distribute chips
+     * 
+     * @param row the current row
+     * @param column the current column
+     */
+    public void ChipDFS(int row, int column) {
+        int chips = grid[row][column];
+        if(grid[row][column] < 4)return;
+
+        //set current and neighbor chips recursively
+        grid[row][column] = chips % 4;  
+        if (row+1 != rows){
+            grid[row+1][column] += chips / 4;  
+            if (grid[row+1][column] > 3)ChipDFS(row+1, column);
+        }
+        if (row-1 >= 0){
+            grid[row-1][column] += chips / 4; 
+            if (grid[row-1][column] > 3)ChipDFS(row-1, column);
+        } 
+        if (column + 1 != columns){
+            grid[row][column+1] += chips / 4; 
+            if (grid[row][column+1] > 3)ChipDFS(row, column+1);
+        } 
+        if (column-1 >= 0){
+            grid[row][column-1] += chips / 4; 
+            if (grid[row][column-1] > 3)ChipDFS(row, column-1);
+        } 
     }
 }
